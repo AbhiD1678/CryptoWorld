cb9f62275942ccc3fd6f3a3e6c0c04984ede92b4
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2023-09-29 18:19:36 +0530

    completed  adding routes now

--- blockchain-webapp/src/App.js
+++ blockchain-webapp/src/App.js
@@ -1,6 +1,9 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import Header from './components/Header'
 import Home from './components/Home'
+import Coins from './components/Coins'
+import Exchanges from './components/Exchanges'
+import CoinDetails from './components/CoinDetails'
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/coins" element={<Coins />}/>
+        <Route path="/exchanges" element={<Exchanges />}/>
+        <Route path="/coins/:id" element={<CoinDetails />}/>
       </Routes>
 
     
