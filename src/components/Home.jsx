433841cf6099b98e4ff15b4ca26a67e438d4410b
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2024-01-10 20:31:29 +0530

    adding footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,26 @@
 import { Text,Box,Image } from '@chakra-ui/react'
 import React from 'react'
-import bg from '../Assets/cryptocurrency-3409656_1280.jpg'
-
+import bg from '../Assets/btc.png'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
-    <Box bgColor={'#000000'} w={'full'} h={'85vh'}>
-      <Image w={'full'} h={'full'} objectFit={'contain'} src={bg} filter={'blackAlpha.800'} />
-
+    <Box bgColor={'blackAlpha.900'} w={'full'} h={'85vh'}>
+      <motion.div
+      style={{
+        height:'80vh'
+      }}
+      animate={{
+        translateY:'20p'
+      }}
+      transition={{
+        duration:2,
+        repeat:Infinity,
+        repeatType:'reverse'
+      }}
+      >
+      <Image w={'full'} h={'full'} objectFit={'contain'} src={bg} filter={'grayscale(1)'} />
+      </motion.div>
       <Text fontSize={'6xl'}
       textAlign={'center'}
       fontWeight={'thin'}
