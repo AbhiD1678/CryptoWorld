0bbf40940943d980ed65178a60f18414a4713aaf
Abhishek Dubey <avdubey_b21@el.vjti.ac.in>
2024-01-10 19:28:14 +0530

    adding home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
+import { Text,Box,Image } from '@chakra-ui/react'
 import React from 'react'
+import bg from '../Assets/cryptocurrency-3409656_1280.jpg'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <Box bgColor={'#000000'} w={'full'} h={'85vh'}>
+      <Image w={'full'} h={'full'} objectFit={'contain'} src={bg} filter={'blackAlpha.800'} />
+
+      <Text fontSize={'6xl'}
+      textAlign={'center'}
+      fontWeight={'thin'}
+      color={'whiteAlpha.700'}
+      mt={'-20'}
+    >
+      CryptoWorld
+    </Text>
+    </Box>
   )
 }
 
